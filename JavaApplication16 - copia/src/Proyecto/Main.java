705a53f29442f7ff88cafb04e7840c6b7bfe2f72
Leoby07 <152244353+Leoby07@users.noreply.github.com>
2023-11-27 21:51:54 -0600

    1-

---
+++ JavaApplication16 - copia/src/Proyecto/Main.java
@@ -0,0 +1,11 @@
+
+package Proyecto;
+
+public class Main {
+    public static void main(String[] args) {
+        Cine v1 = new Cine();
+       
+        v1.setVisible(true); //hacemos visible la ventana
+    }
+}
+
